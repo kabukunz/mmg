aabebe8592f2a0db2ff9e13c0c31d3845d21bb20
kabukunz <kabukunz@gmail.com>
2023-03-17 09:57:42 +0100

    better scripting on windows

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,12 +6,17 @@ cmake -B build -G "Ninja" ^
 -DCMAKE_BUILD_TYPE=Release ^
  ^
 -DBUILD=MMG2D ^
+-DBUILD_SHARED_LIBS=ON ^
 -DLIBMMG2D_STATIC=OFF ^
 -DLIBMMG2D_SHARED=ON ^
 -DUSE_SCOTCH=OFF ^
 -DUSE_ELAS=OFF ^
 -DUSE_VTK=OFF ^
 -DBUILD_TESTING=OFF ^
--DTEST_LIBMMG2D=OFF
+-DTEST_LIBMMG2D=OFF ^
+-DCMAKE_INSTALL_PREFIX:PATH=install
 
 cmake --build build
+cmake --build build --target install
+
+.\build\install\bin\mmg2d_O3.exe .\libexamples\mmg2d\adaptation_example0\example0_a\init.mesh
