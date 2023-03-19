1122bb0f2cd733ea9861dcf1373f27c2602f118a
kabu <kabukunz@gmail.com>
2023-03-19 13:44:46 +0100

    more of it

--- client/build_client.sh
+++ client/build_client.sh
@@ -3,17 +3,22 @@
 # clean
 rm -rf client/build
 rm -rf client/bin
+rm -rf client/init*.*
 
 # build mmg
 client/build_mmg2d.sh
 client/compile_mmg2d.sh
 client/install_mmg2d.sh
 
+cd client
+
 # build client
-cmake -B client/build -G "Ninja" -DCMAKE_BUILD_TYPE=Release
-cmake --build client/build
+cmake -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release
+cmake --build build
 
 # test client
-client/bin/mmg2dclient \
-    libexamples/mmg2d/adaptation_example0/example0_a/init.mesh \
-    client/init_out.mesh
+bin/mmg2dclient \
+    ../libexamples/mmg2d/adaptation_example0/example0_a/init.mesh \
+    init_out.mesh
+
+cd ..
