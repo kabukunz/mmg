d1c7d48396f89e8dd992aafbe7153511bf4f0736
kabu <kabukunz@gmail.com>
2023-03-20 12:33:00 +0100

    pack setup

--- client/pack_mmg2d.sh
+++ client/pack_mmg2d.sh
@@ -2,6 +2,6 @@
 
 mkdir pack
 cd install
-7z a mmg-5.5.2-Darwin-19.5.0.zip lib include
-mv mmg-5.5.2-Darwin-19.5.0.zip ../pack
-cd ../..
+7z a mmg-5.7.0-Darwin-19.5.0.zip lib include bin share
+mv mmg-5.7.0-Darwin-19.5.0.zip ../pack
+cd ..
